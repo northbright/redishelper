6d51845a526053dad0dbbf3a48e829ebf8e6a3df
Frank Xu <frank.xu@northbright.com>
2017-04-17 02:47:46 -0400

    Add GetRedisConn and example.

---
+++ connect_test.go
@@ -0,0 +1,33 @@
+package redishelper_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/northbright/redishelper"
+)
+
+func ExampleGetRedisConn() {
+	var err error
+	var c redis.Conn
+	redisAddr := ":6379"
+	redisPassword := ""
+
+	log.Printf("-------- GetRedisConn() Test Start --------\n")
+
+	if c, err = redishelper.GetRedisConn(redisAddr, redisPassword); err != nil {
+		goto end
+	}
+	defer c.Close()
+
+	log.Printf("Get Redis connection successfully.\n")
+
+end:
+	if err != nil {
+		fmt.Printf("GetRedisConn() error: %v\n", err)
+	}
+
+	log.Printf("-------- GetRedisConn() Test End --------\n\n")
+	// Output:
+}
